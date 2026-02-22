268d08f6fc807ada2c81170d44f8022715920c85
Splitwirez <Splitwirez@gmail.com>
2026-02-22 10:45:01 -0500

    CheckBox and RadioButton

---
+++ samples/ReCap.CommonUI.Demo/Views/Pages/Styles/CheckBoxView.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ReCap.CommonUI.Demo.Views.Pages.Styles
+{
+    public partial class CheckBoxView
+        : UserControl
+    {
+        public CheckBoxView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
