818163fbf82ead21a973459db4273442e9f6c18d
Splitwirez <Splitwirez@gmail.com>
2026-05-14 15:47:29 -0400

    Add styles for `TabStrip` + associated cleanup

---
+++ samples/ReCap.CommonUI.Demo/Views/Pages/Styles/TabStripView.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ReCap.CommonUI.Demo.Views.Pages.Styles
+{
+    public partial class TabStripView
+        : UserControl
+    {
+        public TabStripView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
