18b088fa42a399deb47e9d41210b37c4010e7096
Splitwirez <Splitwirez@gmail.com>
2026-05-29 14:06:13 -0400

    Menu bar styles

---
+++ samples/ReCap.CommonUI.Demo/Views/Pages/Styles/MenuView.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ReCap.CommonUI.Demo.Views.Pages.Styles
+{
+    public partial class MenuView
+        : UserControl
+    {
+        public MenuView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
